55de6cbeb1ce9b5f928a2beeccdc1ac2358be957
Jonathan Chin-Sue <jchinsue@gmail.com>
2018-11-26 17:23:37 +0100

    Added controller and CSS/JS files

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexController = require('./controllers/index');
+var usersController = require('./controllers/users');
 
 var app = express();
 
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexController);
+app.use('/users', usersController);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +39,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+app.listen(3000);
